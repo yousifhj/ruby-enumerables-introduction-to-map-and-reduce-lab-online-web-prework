390e77737e4c0fcfc2724e830e77f1874b253f65
Huda Yousif <hjdoski@gmail.com>
2019-09-25 22:02:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ end
   def reduce_to_all_true(array)
   counter = 0 
   while counter < array.size do
-    return false if ()
+    return false if !condition
     counter += 1
   end
   return false 
